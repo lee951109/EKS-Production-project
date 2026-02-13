af6eae326c4caea7171d1440fa438752d07ee012
이지현 (イジヒョン) <71690561+lee951109@users.noreply.github.com>
2026-02-13 22:31:46 +0900

    eks setup with alb, ssl and domain connection

---
+++ infra-iac/route53_acm.tf
@@ -0,0 +1,40 @@
+# 1. Route 53 호스팅 영역 (도메인 관리소)
+resource "aws_route53_zone" "main" {
+  name = "fastcampus-jihyun.link"
+}
+
+# 2. ACM 인증서 신청
+resource "aws_acm_certificate" "cert" {
+  domain_name = "fastcampus-jihyun.link"
+  validation_method = "DNS"
+
+  subject_alternative_names = ["*.fastcampus-jihyun.link"]
+
+
+  lifecycle {
+    # 인증서 갱신 시 새인증서를 먼저 만들고 기존 것 삭제
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "eks-app-cert"
+  }
+}
+
+# 3. 도메인 소유권 확인을 위한 레코드 자동 생성
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.main.zone_id
+}
